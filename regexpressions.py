fcf7018085e7a549a65a44a6a19767d1ee439156
Ahsan Shahid <ahsanshahid2882@gmail.com>
2024-01-08 19:25:48 +0500

    Update regexpressions.py

--- regexpressions.py
+++ regexpressions.py
@@ -1,7 +1,4 @@
-from pprint import pprint
-import re
-import os
-from typing import Dict, List, Union
+from typing import Dict, FrozenSet, List, Union
 
 
 class Node:
@@ -51,22 +48,10 @@ class RegExpression:
         self.CONCAT = '.'
         self.UNION = '+'
 
-    def convert_to_tree(self, exp: str):
-        def tree_to_json(node: Node):
-            if node is None:
-                return None
-            if node.operand.isalnum():
-                return {'value': node.operand}
-            left_tree = tree_to_json(node.left)
-            right_tree = tree_to_json(node.right)
-            nnode = {"value": node.operand, "left": left_tree}
-            if right_tree:
-                nnode["right"] = right_tree
-            return nnode
-
+    def __convert_to_tree(self, exp: str) -> Node:
         stack: Union[List[Node], str] = []
         root: Node = self.__update_tree(stack, exp)
-        return tree_to_json(root)
+        return root
 
     def __update_tree(self, stack: Union[List[Node], str], exp: str) -> Node:
         def reduce_group(group: List[Node], reverse: bool = False):
@@ -87,7 +72,7 @@ class RegExpression:
                             node.left = group.pop()
                             node.right = last
                             group.append(node)
-                    elif group[-1].operand.isalnum():
+                    elif group[-1].operand.isalnum() or group[-1].operand in ['*']:
                         node = Node('.')
                         node.left = group.pop()
                         node.right = last
@@ -101,9 +86,9 @@ class RegExpression:
 
         for char in exp:
             match char:
-                case self.RIGHT_PAREN:
-                    stack.append(char)
                 case self.LEFT_PAREN:
+                    stack.append(char)
+                case self.RIGHT_PAREN:
                     # pop all chars until stack has '('
                     group: List[Node] = []
                     while stack and stack[-1] != '(':
@@ -133,3 +118,61 @@ class RegExpression:
                 case _:
                     stack.append(Node(char))
         return reduce_group(stack)
+
+    def __build_FA_from_node(self, node: Node, states: List[str],
+                             start_state: str, accept_states: List[str],
+                             transitions: Dict[FrozenSet[str], List[str]],
+                             alphabets: List[str], initial_state: str, final_state: str):
+        def create_new_state():
+            return f"Q{len(states)}"
+
+        if node.operand == self.UNION:
+            # accept_states.append(new_state)
+            (transitions, alphabets) = self.__build_FA_from_node(
+                node.left, states, start_state, accept_states, transitions, alphabets, initial_state, final_state)
+            (transitions, alphabets) = self.__build_FA_from_node(
+                node.right, states, start_state, accept_states, transitions, alphabets, initial_state, final_state)
+        elif node.operand == self.KLEENE:
+            eps_state_1 = create_new_state()
+            states.append(eps_state_1)
+            key = frozenset({initial_state, 'ε'})
+            if key in transitions:
+                transitions[key].append(eps_state_1)
+            else:
+                transitions[key] = [eps_state_1]
+            # eps_state_2 = create_new_state()
+            key = frozenset({eps_state_1, 'ε'})
+            if key in transitions:
+                transitions[key].append(final_state)
+            else:
+                transitions[key] = [final_state]
+            (transitions, alphabets) = self.__build_FA_from_node(
+                node.left, states, start_state, accept_states, transitions, alphabets, eps_state_1, eps_state_1)
+        elif node.operand == self.CONCAT:
+            new_state = create_new_state()  # q2
+            states.append(new_state)
+            (transitions, alphabets) = self.__build_FA_from_node(
+                node.left, states, start_state, accept_states, transitions, alphabets, initial_state, final_state=new_state)
+            (transitions, alphabets) = self.__build_FA_from_node(node.right, states, start_state, accept_states,
+                                                                 transitions, alphabets, initial_state=new_state, final_state=final_state)
+        elif node.operand.isalnum():
+            alphabets.append(node.operand)
+            alphabets = list(set(alphabets))
+            key = frozenset({initial_state, node.operand})
+            if key in transitions:
+                transitions[key].append(final_state)
+            else:
+                transitions[key] = [final_state]
+        return (transitions, alphabets)
+
+    def convert_to_FA(self, regexp: str):
+        root: Node = self.__convert_to_tree(regexp)
+        start_state = 'Q0'
+        accept_state: str = 'Q1'
+        states: List[str] = [start_state, accept_state]
+        transitions: Dict[FrozenSet[str], List[str]] = {}
+        alphabets: List[str] = []
+        (transitions, alphabets) = self.__build_FA_from_node(
+            root, states, start_state, accept_state, transitions, alphabets, start_state, final_state=accept_state)
+        from nfa import eNFA
+        return eNFA(states=states, alphabet=alphabets, transitions=transitions, start_state=start_state, accept_states=[accept_state])
