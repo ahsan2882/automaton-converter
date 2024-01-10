02ac243b868cd44041cc745158095e2541088c51
Ahsan Shahid <ahsanshahid2882@gmail.com>
2024-01-10 22:23:36 +0500

    basic UI home

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  UntypedFormBuilder,
-  UntypedFormControl,
-  FormGroup,
-} from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 
 enum FA {
   NFA = 'NFA',
@@ -23,11 +19,23 @@ export class HomeComponent implements OnInit {
   dropdownControl = new UntypedFormControl(this.dropdownItems[0]);
   selectedFA = this.dropdownItems[0];
 
+  fa_alphabetControl = new UntypedFormControl('');
+  fa_statesControl = new UntypedFormControl('');
+  fa_startState = new UntypedFormControl('');
+  fa_acceptStates = new UntypedFormControl('');
+  fa_transitionsControl = new UntypedFormControl('');
+
+  finiteAutomatonFeatures = new UntypedFormGroup({
+    alphabet: this.fa_alphabetControl,
+    states: this.fa_statesControl,
+    startState: this.fa_startState,
+    acceptStates: this.fa_acceptStates,
+    transitions: this.fa_transitionsControl,
+  });
+
   ngOnInit(): void {
     this.dropdownControl.valueChanges.subscribe((value: FA) => {
-      // Do something with the selected value
       this.selectedFA = value;
-      console.log('Selected value:', value);
     });
   }
 }
