0682b116db7d0daa657f265eb987d86a37672fce
Ahsan Shahid <ahsanshahid2882@gmail.com>
2024-01-10 20:26:45 +0500

    update

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormControl, FormGroup } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormControl,
+  FormGroup,
+} from '@angular/forms';
 
 enum FA {
   NFA = 'NFA',
@@ -18,7 +22,6 @@ export class HomeComponent implements OnInit {
   dropdownItems = [FA.NFA, FA.E_NFA, FA.DFA, FA.REGEXP];
   dropdownControl = new UntypedFormControl(this.dropdownItems[0]);
   selectedFA = this.dropdownItems[0];
-  constructor(private formBuilder: UntypedFormBuilder) {}
 
   ngOnInit(): void {
     this.dropdownControl.valueChanges.subscribe((value: FA) => {
