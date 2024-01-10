da614352711d91117c2b7a689ca05f74b9af89e7
Ahsan Shahid <ahsanshahid2882@gmail.com>
2024-01-10 20:05:29 +0500

    basic client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+enum FA {
+  NFA = 'NFA',
+  E_NFA = 'Epsilon-NFA',
+  DFA = 'DFA',
+  REGEXP = 'RegExp',
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  automaton = FA;
+  dropdownItems = [FA.NFA, FA.E_NFA, FA.DFA, FA.REGEXP];
+  dropdownControl = new FormControl(this.dropdownItems[0]);
+  selectedFA = this.dropdownItems[0];
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.dropdownControl.valueChanges.subscribe((value: FA) => {
+      // Do something with the selected value
+      this.selectedFA = value;
+      console.log('Selected value:', value);
+    });
+  }
+}
